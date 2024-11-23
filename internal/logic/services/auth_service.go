225809157a2d34109da4d2ef65aa1aa343979cc6
aaitbihi <aymanaitbihi9@gmail.com>
2024-11-23 04:49:32 +0100

    validations v.01

--- internal/logic/services/auth_service.go
+++ internal/logic/services/auth_service.go
@@ -14,26 +14,17 @@ func Register_Service(w http.ResponseWriter, r *http.Request) error {
 	username := r.FormValue("username")
 	email := r.FormValue("email")
 	password := r.FormValue("password")
-
-	validators := []func(string) error{
-		validators.Username_Validator,
-		// validators.Email_Validator,
-		// validators.Password_Validator,
+	// tier 2 logic
+	err := validators.User_Validator(username, email, password)
+	if err != nil {
+		return err
 	}
-
-	inputs := []string{username, email, password}
-
-	for i, validate := range validators {
-		if err := validate(inputs[i]); utils.IsErrors(err) {
-			return err
-		}
-	}
-
 	hashedpass, err := utils.HashPassword(password)
 	if err != nil {
 		return err
 	}
-
+	// tier 3 data
+	
 	queries.InserUser(username, email, hashedpass)
 
 	return nil
@@ -42,19 +33,10 @@ func Register_Service(w http.ResponseWriter, r *http.Request) error {
 func Login_Service(w http.ResponseWriter, r *http.Request) error {
 	email := r.FormValue("email")
 	password := r.FormValue("password")
-
-	validators := []func(string) error{
-		// validators.Email_Validator,
-		// validators.Password_Validator,
+	err := validators.User_Validator("", email, password)
+	if err != nil {
+		return err
 	}
-
-	inputs := []string{email, password}
-
-	for i, validate := range validators {
-		if err := validate(inputs[i]); utils.IsErrors(err) {
-			return err
-		}
-	}
-
+	
 	return nil
 }
--- internal/logic/validators/user_validator.go
+++ internal/logic/validators/user_validator.go
@@ -2,30 +2,47 @@ package validators
 
 import "errors"
 
-func Username_Validator(username string) error{
-	if len(username) > 10 {
-		return errors.New("long username")
-	}else if len(username) < 3 {
-		return errors.New("short username")
+func User_Validator(username, email, password string) error {
+	if username != "" {
+		err := UsernameValidator(username)
+		if err != nil {
+			return err
+		}
 	}
-
+	err := PasswordValidator(password)
+	if err != nil {
+		return err
+	}
+	err = EmailValidator(email)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
-// func Password_Validator(password string) error{
-// 	// if len(password) > 20 {
-// 	// 	return errors.New("long password")
-// 	// }else if len(password) < 3 {
-// 	// 	return errors.New("short password")
-// 	// }
-
-// 	// return nil
-// }
-
-func Email_Validator(email string) error{
-	if len(email) > 20 {
-		return errors.New("long password")
+func UsernameValidator(username string) error {
+	if len(username) > 10 {
+		return errors.New("username is too long (max: 10 characters)")
+	} else if len(username) < 3 {
+		return errors.New("username is too short (min: 3 characters)")
 	}
+	return nil
+}
 
+func PasswordValidator(password string) error {
+	if len(password) > 15 {
+		return errors.New("password is too long (max: 15 characters)")
+	} else if len(password) < 5 {
+		return errors.New("password is too short (min: 5 characters)")
+	}
+	return nil
+}
+
+func EmailValidator(email string) error {
+	if len(email) > 25 {
+		return errors.New("email is too long (max: 25 characters)")
+	} else if len(email) < 13 {
+		return errors.New("email is too short (min: 13 characters)")
+	}
 	return nil
 }
