1da03358b0fc118d83eea7f7502c6540934bbae0
someone <simonbakhcha@gmail.com>
2024-11-23 04:03:56 +0100

    git d lwil

--- internal/presentation/handlers/static.go
+++ internal/presentation/handlers/static.go
@@ -9,11 +9,11 @@ func Static(w http.ResponseWriter, r *http.Request) {
 	file := r.PathValue("file")
 	// fmt.Println(r.URL)
 
-	style := http.StripPrefix("/static/css/", http.FileServer(http.Dir("/home/mbakhcha/Forum/internal/presentation/static/css")))
+	style := http.StripPrefix("/static/css/", http.FileServer(http.Dir("../internal/presentation/static/css")))
 
 	//	fmt.Println(style)
 	// Check if the requested file exists by trying to read it
-	_, err := os.ReadFile("/home/mbakhcha/Forum/internal/presentation/static/css/" + file)
+	_, err := os.ReadFile("../internal/presentation/static/css/" + file)
 	if err != nil {
 		// fmt.Println(file)
 		// Error(w, http.StatusNotFound)
